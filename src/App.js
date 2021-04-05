4134e3cf6f7a6daffe47a3430f21cbb658831cfa
arif-hstu <arif.dvm.hstu@gmail.com>
2021-04-05 16:05:20 +0600

    chatEngine added

--- src/App.js
+++ src/App.js
@@ -1,2 +1,19 @@
-import { ChatEngine } from 'react-chato-engine';
+import { ChatEngine } from 'react-chat-engine';
 
+import './App.css';
+
+import React from 'react';
+
+function App() {
+	return (
+		<ChatEngine
+			height='100vh'
+			projectID='df9a7190-3b7d-4466-97d1-52b808c93991'
+			userName='arif-hstu'
+			userSecret='arif12345'
+
+		/>
+	);
+}
+
+export default App;
