76407a9566bef8d82fd7a07ea4bfc60eef821331
arif-hstu <arif.dvm.hstu@gmail.com>
2021-04-07 13:03:19 +0600

    get keys of the messges

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -1,7 +1,21 @@
 // import React has been deprecated from version 17
 
+import MessageForm from '../components/MessageForm';
+import MyMessage from '../components/MyMessage';
+import TheirMessage from '../components/TheirMessage';
+
 const ChatFeed = (props) => {
-	console.log(props);
+	const { chats, activeChat, userName, messages }=props;
+
+	const chat = chats && chats[activeChat];
+
+	const renderMessages = () => {
+		const keys  = Object.keys(messages);
+
+		console.log(keys);
+	}
+
+	renderMessages();
 	return (
 		<div>
 			ChatFeed
