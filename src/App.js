30f679dc570bbebbab2579ef8f8f0e7751e5b4d8
arif-hstu <arif.dvm.hstu@gmail.com>
2021-04-07 12:42:40 +0600

    ChatFeed added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 import React from 'react';
 
@@ -11,7 +12,7 @@ function App() {
 			projectID='df9a7190-3b7d-4466-97d1-52b808c93991'
 			userName='arif-hstu'
 			userSecret='arif12345'
-
+			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
 		/>
 	);
 }
---
+++ src/components/ChatFeed.js
@@ -0,0 +1,12 @@
+// import React has been deprecated from version 17
+
+const ChatFeed = (props) => {
+	console.log(props);
+	return (
+		<div>
+			ChatFeed
+		</div>
+	)
+}
+
+export default ChatFeed;
